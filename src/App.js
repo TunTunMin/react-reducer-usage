9839a47fa34b4b457358004461e01c8f0a81ef35
TunTunMin <tun2min@cloudsource.co.jp>
2023-01-17 10:22:07 +0630

    react useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from 'react';
+// import './App.css';
 
+const intialValue = { people: []};
+
+function reducer(state, action){
+  if(action.type === "INPUT_ADDED"){
+    const newPeople = [...state.people, action.payload];
+    return { ...state, people: newPeople};
+  }
+}
 function App() {
+  
+  const [inputValue,setInputValue] = useState("");
+  const [state, dispatch] = useReducer(reducer, intialValue);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newItem = {id: Math.random().toString(), name: inputValue}
+    dispatch({type: "INPUT_ADDED", payload: newItem});
+    setInputValue("");
+  }
+  console.log(state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>FORM</h1>
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Enter Name" onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
+        <button type="submit">Save</button>
+      </form>
+      <ul>
+        {state.people.map((person) => {
+          return <li key={person.id}>{person.name}</li>
+        })}
+      </ul>
     </div>
   );
 }
